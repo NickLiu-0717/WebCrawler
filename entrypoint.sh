913c2e8198fbe5ac3aeb49d3d8606b74c7a555d4
NickLiu-0717 <glrt2793@gmail.com>
2025-04-05 16:56:31 +0800

    modify dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# GOOSE=$(which goose)
-
-# echo "Running database migrations..."
-# $GOOSE -dir ./sql/schema postgres "$DATABASE_URL" up || (echo "Migration failed, retrying in 5s..." && sleep 5 && $GOOSE -dir ./sql/schema postgres "$DATABASE_URL" up)
-
-# echo "DATABASE_URL is: $DATABASE_URL"
-
-# echo "Starting Webcrawler..."
-# exec ./crawler
-
 GOOSE=$(which goose)
 
 # Compose database URL
